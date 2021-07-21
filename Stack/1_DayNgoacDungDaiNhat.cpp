c41a344d4fbfd92bf587eac14861568d2029321b
manhtung001 <khongtung001@gmail.com>
2021-07-21 20:44:54 +0700

    add func nothing

--- Stack/1_DayNgoacDungDaiNhat.cpp
+++ Stack/1_DayNgoacDungDaiNhat.cpp
@@ -2,35 +2,50 @@
 using namespace std;
 typedef long long ll;
 
+int checkSum(int n)
+{
+    int sum = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (n % i == 0)
+            sum += i;
+        else
+            sum = sum - i;
+    }
+    if (sum % 2 == 0)
+        return sum;
+    return sum + 1;
+}
+
 main()
 {
- int t;
- cin >> t;
- while (t--)
- {
-  string s;
-  cin >> s;
-  ll n = s.size();
-  stack<ll> st;
-  st.push(-1);
-  ll res = 0;
-  for (ll i = 0; i < n; i++)
-  {
-   if (s[i] == '(')
-   {
-    st.push(i);
-   }
-   else
-   {
-    st.pop();
-    if (st.size() == 0)
-     st.push(i);
-    else
+    int t;
+    cin >> t;
+    while (t--)
     {
-     res = max(res, i - st.top());
+        string s;
+        cin >> s;
+        ll n = s.size();
+        stack<ll> st;
+        st.push(-1);
+        ll res = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if (st.size() == 0)
+                    st.push(i);
+                else
+                {
+                    res = max(res, i - st.top());
+                }
+            }
+        }
+        cout << res << endl;
     }
-   }
-  }
-  cout << res << endl;
- }
 }
